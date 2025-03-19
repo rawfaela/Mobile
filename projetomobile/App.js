1021935385fe036c8f2ff7db14f98e72d78d1288
rafa <rafaelaaghellere@gmail.com>
2025-03-19 17:17:38 -0300

    teste

--- projetomobile/App.js
+++ projetomobile/App.js
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 import Home from './screens/home';
 import Login from './screens/login';
+import 'react-native-gesture-handler';
 //comentar um e descomentar o outro pra ver certo (se nao a tela fica dividida)
 
 export default function App() { 
